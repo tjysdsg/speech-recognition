83d8eb91650cdb0b672be87a0c13fc03bfb312d5
tjysdsg <tjy1018543509@163.com>
2019-08-02 19:25:50 +0800

    Add CLI tools for training, testing and recording.

--- src/config.py
+++ src/config.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 data_path = "/home/tjy/repos/aurora-digits-single"
 digit_names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'O', 'Z']
-do_train = False
-do_test = False
 
 use_multiprocessing = True
