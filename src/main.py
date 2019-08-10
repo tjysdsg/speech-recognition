9822c10d14f65e9f4e3fbde77e4c0f80078b2bd8
tjysdsg <tjy1018543509@163.com>
2019-08-10 16:51:11 +0800

    Fix continuous speech training

--- src/main.py
+++ src/main.py
@@ -96,120 +96,25 @@ def test(models, folder, file_patterns):
 
 
 if __name__ == "__main__":
-    if False:
-        # continuous speech recognition
-        # get all the models from pickle files
-        models = []
-        for digit in digit_names:
-            file = open('models/' + digit + '.pkl', 'rb')
-            models.append(pickle.load(file))
-            file.close()
-
-        model_graph = LayeredHMMGraph()
-        nes0 = model_graph.add_non_emitting_state()
-        model_graph.add_layer_from_models(models[1:9])
-        model_graph.add_non_emitting_state()
-        model_graph.add_layer_from_models(models)
-        model_graph.add_non_emitting_state()
-        model_graph.add_layer_from_models(models)
-        nes1 = model_graph.add_non_emitting_state()
-
-        model_graph.add_edge(nes0, nes1)
-
-        model_graph.add_layer_from_models(models)
-        model_graph.add_non_emitting_state()
-        model_graph.add_layer_from_models(models)
-        model_graph.add_non_emitting_state()
-        model_graph.add_layer_from_models(models)
-        model_graph.add_non_emitting_state()
-        model_graph.add_layer_from_models(models)
-        model_graph.add_non_emitting_state()
-
-        # record('test.wav')
-        # get all test files using regular expressions
-        f = 'test.wav'
-        x = load_wav_as_mfcc(f)
-        _, matched = sentence_viterbi(x, model_graph)
-        print(matched)
-
     if True:
         models = []
         for digit in digit_names:
             file = open('models-4gaussians-em-realign/' + digit + '.pkl', 'rb')
             models.append(pickle.load(file))
             file.close()
-        filenames = ['FAC_13A',
-                     'FAC_1473533A',
-                     'FAC_172A',
-                     'FAC_1911446A',
-                     'FAC_1A',
-                     'FAC_1B',
-                     'FAC_1O1A',
-                     'FAC_24Z52A',
-                     'FAC_26555A',
-                     'FAC_282A',
-                     'FAC_29A',
-                     'FAC_29OA',
-                     'FAC_2A',
-                     'FAC_2B',
-                     'FAC_32421A',
-                     'FAC_33O31A',
-                     'FAC_369O5A',
-                     'FAC_37O1641A',
-                     'FAC_39ZA',
-                     'FAC_3A',
-                     'FAC_3B',
-                     'FAC_434A',
-                     'FAC_43A',
-                     'FAC_47A',
-                     'FAC_4876A',
-                     'FAC_4915A',
-                     'FAC_4A',
-                     'FAC_4B',
-                     'FAC_54A',
-                     'FAC_5821A',
-                     'FAC_59Z2A',
-                     'FAC_5A',
-                     'FAC_5B',
-                     'FAC_5Z26ZA',
-                     'FAC_5Z31ZZ4A',
-                     ]
 
-        transcripts = ['one three',
-                       'one four seven three five three three',
-                       'one seven two',
-                       'one nine one one four four six',
-                       'one',
-                       'one',
-                       'one oh one',
-                       'two four zero five two',
-                       'two six five five five',
-                       'two eight two',
-                       'two nine',
-                       'two nine oh',
-                       'two',
-                       'two',
-                       'three two four two one',
-                       'three three oh three one',
-                       'three six nine oh five',
-                       'three seven oh one six four one',
-                       'three nine zero',
-                       'three',
-                       'three',
-                       'four three four',
-                       'four three',
-                       'four seven',
-                       'four eight seven six',
-                       'four nine one five',
-                       'four',
-                       'four',
-                       'five four',
-                       'five eight two one',
-                       'five nine zero two',
-                       'five',
-                       'five',
-                       'five zero two six zero',
-                       'five zero three one zero zero four']
+        # get filenames
+        f = open('/home/tjy/repos/aurora_digits/TRAIN.filelist')
+        filenames = [line.rstrip('\n\r ') for line in f]
+        f.close()
+        # get transcripts
+        import re
+
+        ex = re.compile(r'^[A-Za-z\s]+')
+        f = open('/home/tjy/repos/aurora_digits/TRAIN.transcripts')
+        transcripts = [ex.match(line)[0].rstrip('\n\r ') for line in f]
+        transcripts = [t.replace('sil', '') for t in transcripts]
+        f.close()
 
         digit_name_idx_map = {'one': 0,
                               'two': 1,
@@ -224,7 +129,7 @@ if __name__ == "__main__":
                               'zero': 10}
 
         print('loading data')
-        data = np.array([load_wav_as_mfcc(os.path.join(data_path, 'train', f + '.wav')) for f in filenames])
+        data = [load_wav_as_mfcc(os.path.join(data_path, 'train', f + '.wav')) for f in filenames]
         print('loading transcripts')
 
         labels = [list(map(lambda x: digit_name_idx_map[x], t.split())) for t in transcripts]
@@ -233,14 +138,4 @@ if __name__ == "__main__":
         print(labels)
         print('=' * 25)
 
-        # make model_graphs
-        model_graphs = [ContinuousGraph([]) for _ in labels]
-        n_labels = len(labels)
-        for i in range(n_labels):
-            lb = labels[i]
-            model_graphs[i].add_non_emitting_state()
-            for digit in lb:
-                model_graphs[i].add_model(models[digit], digit)
-                model_graphs[i].add_non_emitting_state()
-
-        continuous_train(data, model_graphs, labels)
+        continuous_train(data, models, labels)
--- src/sr/recognition/continuous_speech.py
+++ src/sr/recognition/continuous_speech.py
@@ -7,31 +7,52 @@ import os
 import pickle
 
 
-def continuous_train(data, model_graphs, word_indices, use_gmm=True, n_gaussians=5, use_em=False):
-    print('rearranging data for continuous training...')
-    segments = {w: [] for w in chain.from_iterable(word_indices)}
+def continuous_train(data, models, word_indices, use_gmm=True, n_gaussians=4, use_em=False, max_iteration=1):
+    # remember old models
+    old_models = models
+    for iter in range(max_iteration):
+        converged = True
+        print('=' * 25)
+        print('continuous training iteration:', iter)
+        print('building model graphs')
+        # make model_graphs
+        model_graphs = [ContinuousGraph([]) for _ in word_indices]
+        n_labels = len(word_indices)
+        for i in range(n_labels):
+            lb = word_indices[i]
+            model_graphs[i].add_non_emitting_state()
+            for digit in lb:
+                model_graphs[i].add_model(models[digit], digit)
+                model_graphs[i].add_non_emitting_state()
 
-    data_len = len(data)
-    for i in range(data_len):
-        m = model_graphs[i]
-        x = data[i]
-        # w = word_indices[i]
-        _, path = sentence_viterbi(x, m)
-        # find segments for every digit, a segment is ended and started by 'NES'
-        # each segment is a sequence of mfcc features, thus it's a 2d array-like
-        i_nes = [i for i, x in enumerate(path) if x == "NES"]
-        i_nes_len = len(i_nes)
-        for i in range(i_nes_len - 1):
-            start_i = i_nes[i] + 1
-            segments[path[start_i]].append(np.array(x[start_i:i_nes[i + 1]]))
+        print('rearranging data for continuous training...')
+        segments = {w: [] for w in chain.from_iterable(word_indices)}
+        data_len = len(data)
+        for i in range(data_len):
+            m = model_graphs[i]
+            x = data[i]
+            # w = word_indices[i]
+            _, path = sentence_viterbi(x, m)
+            # find segments for every digit, a segment is ended and started by 'NES'
+            # each segment is a sequence of mfcc features, thus it's a 2d array-like
+            i_nes = [i for i, x in enumerate(path) if x == "NES"]
+            i_nes_len = len(i_nes)
+            for i in range(i_nes_len - 1):
+                start_i = i_nes[i] + 1
+                segments[path[start_i]].append(np.array(x[start_i:i_nes[i + 1]]))
 
-    # continuous training
-    print('doing continuous training...')
-    print('=' * 25)
-    for w in segments.keys():
-        # train a new HMM model using the segments
-        m = HMM(n_gaussians)
-        m.fit(segments[w], n_gaussians, use_gmm, use_em)
-        file = open(os.path.join('models', str(w) + '.pkl'), 'wb')
-        pickle.dump(m, file)
-        file.close()
+        # continuous training
+        print('doing hmm training...')
+        for w in segments.keys():
+            # train a new HMM model using the segments
+            m = HMM(5)
+            m.fit(segments[w], n_gaussians, use_gmm, use_em)
+            converged = converged and m == old_models[w]
+            old_models[w] = m
+            file = open(os.path.join('models', str(w) + '.pkl'), 'wb')
+            pickle.dump(m, file)
+            file.close()
+
+        if converged:
+            print('continuous training converged')
+            break
