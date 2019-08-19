db1d75bed685f4adb651bd16358028f8131ce66b
tjysdsg <tjy1018543509@163.com>
2019-08-19 22:45:51 +0800

    Refactor, fix stuff

--- src/sr/recognition/continuous_speech.py
+++ src/sr/recognition/continuous_speech.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from .decode import decode_hmm_states
-from .hmm import HMM
-from .hmm_state import NES
-from itertools import chain
+from .kmeans import kmeans
+from .hmm_state import GMM, NES, mahalanobis
 import os
 import pickle
 import warnings
@@ -38,72 +37,101 @@ def build_state_sequences(hmms, label_matrix):
     return seq, trans, trans_offset - 1
 
 
-def continuous_train(data, models, label_seqs, use_gmm=True, n_gaussians=4, use_em=True, max_iteration=1000):
+def continuous_train(data, models, label_seqs, n_gaussians=4, max_iteration=1000):
     # remember old models
-    old_models = copy.deepcopy(models)
+    old_models = models
+    new_models = copy.deepcopy(models)
 
     for iter in range(max_iteration):
-        converged = True
         print('=' * 25)
         print('Continuous training iteration:', iter)
         print('Rearranging data for hmm training...')
-        segments = {w: [] for w in chain.from_iterable(label_seqs)}
+        segments = {}
         data_len = len(data)
 
         print('Building state sequences')
-        sequences_and_transitions = [build_state_sequences(old_models, [[l] for l in labels]) for labels in label_seqs]
+        sequences_and_transitions = [build_state_sequences(new_models, [[l] for l in labels]) for labels in label_seqs]
 
         print('Rearranging data, this may take a while...')
         for i in range(data_len):
+            print('Progress:', str(int(100 * i / data_len)) + "%", end='\r', flush=True)
             seq = sequences_and_transitions[i][0]
             trans = sequences_and_transitions[i][1]
             end_idx = sequences_and_transitions[i][2]
             x = data[i]
 
             _, path = decode_hmm_states(x, seq, trans, end_points=[[end_idx, -1]])
-            # FIXME
-            path = reversed(path[:, 0].tolist())
-
-            print('Progress:', str(int(100 * i / data_len)) + "%", end='\r')
-
-            # find segments for every digit, a segment is ended and started by 'NES'
-            # each segment is a sequence of mfcc features, thus it's a 2d array-like
-            i_nes = [i for i, x in enumerate(path) if x == "NES"]
-            i_nes_len = len(i_nes)
-            for index in range(i_nes_len - 1):
-                start_i = i_nes[index] + 1
-                segments[path[start_i]].append(x[start_i:i_nes[index + 1]])
+            path = list(reversed(path.tolist()))
+            start_idx = None
+            for pt in path:
+                r = pt[0]
+                c = pt[1]
+                if start_idx is None:
+                    start_idx = c
+                if type(seq[r]) is NES and start_idx < c:
+                    if segments.get(seq[r]) is None:
+                        segments[seq[r]] = [x[start_idx: c]]
+                    else:
+                        segments[seq[r]].append(x[start_idx: c])
+                        start_idx = None
 
         print('Complete data rearrangement')
         print("=" * 25)
 
         # remove templates that are too small to do dtw on
-        for w in segments.keys():
-            seg = segments[w]
-            seg_len = len(seg)
-            del_i = []
-            for si in range(seg_len):
-                if seg[si].shape[0] < 5:
-                    warnings.warn('Removing digit templates that are too small', UserWarning)
-                    del_i.append(si)
+        # for w in segments.keys():
+        #     seg = segments[w]
+        #     seg_len = len(seg)
+        #     del_i = []
+        #     for si in range(seg_len):
+        #         if seg[si].shape[0] < 5:
+        #             warnings.warn('Removing digit templates that are too small', UserWarning)
+        #             del_i.append(si)
 
-            del_i.sort(reverse=True)
-            for si in del_i:
-                del seg[si]
+        #     del_i.sort(reverse=True)
+        #     for si in del_i:
+        #         del seg[si]
 
         # continuous training
         print('Doing HMM training...')
-        for w in segments.keys():
-            # train a new HMM model using the segments
-            m = HMM(5)
-            m.fit(segments[w], n_gaussians, use_gmm, use_em)
-            converged = converged and m == old_models[w]
-            old_models[w] = m
-            # TODO: use command line argument for output model path
-            file = open(os.path.join('models-continuous-4gaussians-em-realign', str(w) + '.pkl'), 'wb')
-            pickle.dump(m, file)
-            file.close()
+        for state in segments.keys():
+            seg = segments[state]
 
-        if converged:
+            # train gmm states
+            n_splits = int(np.log(n_gaussians))
+            assert n_splits > 0
+
+            mu = np.mean(seg, axis=1)
+            centroids = np.array([mu])
+            weights = np.full(n_gaussians, 1 / data.shape[0])
+            for i in range(n_splits):
+                lcentroids = centroids * 0.9
+                hcentroids = centroids * 1.1
+                centroids = np.concatenate([lcentroids, hcentroids], axis=0)
+                clusters, centroids, variance = kmeans(data, 2 ** (i + 1), centroids, dist_fun=mahalanobis)
+
+                # calculate and update weights of distributions
+                cs, c_counts = np.unique(clusters, return_counts=True)
+                for c in cs:
+                    weights[c] = c_counts[c] / data.shape[0]
+
+                # update model parameters
+                state.update_models(centroids, variance, weights[:2 ** (i + 1)])
+                # Expectation-maximization
+                state.em(data, 2 ** (i + 1))
+
+        # save new models to files
+        # TODO: use command line argument for output model path
+        for i in range(len(new_models)):
+            file = open(os.path.join('models-continuous-4gaussians-em-realign', str(i) + '.pkl'), 'wb')
+            pickle.dump(new_models[i], file)
+            file.close()
+        # check if converged
+        convergence = [new_m == old_m for new_m, old_m in zip(new_models, old_models)]
+        converged = np.alltrue(convergence)
+        if not converged:
+            old_models = new_models
+            new_models = copy.deepcopy(old_models)
+        else:
             print('Continuous training converged')
             break
